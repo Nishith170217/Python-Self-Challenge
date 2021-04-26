9b05fbb7b7c8bb5a72b36ba90c09e2d7d0a0448c
Nishith <45823099+Nishith170217@users.noreply.github.com>
2021-04-26 17:51:36 +0800

    Add files via upload

---
+++ Python program to add 2 numbers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def add(a,b):
+    sum = a+b
+    print(sum)
+
+add(2,3)
+    
---
+++ maximum between 2 numbers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 17:44:40 2021
+
+@author: Nishith
+"""
+
+def maximum(x,y):
+    if x>y:
+        max=x
+    else:
+        max=y
+    print(max)
+    
+maximum(34,2)
