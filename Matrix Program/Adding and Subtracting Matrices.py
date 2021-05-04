797865db980cf4e73afe1ba615fe9d5afd021154
Nishith <45823099+Nishith170217@users.noreply.github.com>
2021-05-04 16:31:23 +0800

    Add files via upload

---
+++ Matrix Program/Adding and Subtracting Matrices.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  2 10:58:09 2021
+
+@author: Nishith
+"""
+
+import numpy as np
+A = [[1,2],[3,4]]
+B = [[4,5],[6,7]]
+print("Adding array: \n",np.add(A,B))
+
+print("Subtracting array:\n",np.subtract(A,B))
