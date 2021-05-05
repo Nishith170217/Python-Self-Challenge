b9a230e35b5b34c3ccfe27d06cd6e6189bed17df
Nishith <45823099+Nishith170217@users.noreply.github.com>
2021-05-05 18:00:35 +0800

    Add files via upload

---
+++ String problem/binary string check.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  5 17:50:18 2021
+
+@author: Nishith
+"""
+
+st="110211"
+ss=set(st)
+s={'1','0'}
+if(s==ss or ss=='0' or ss=='1'):
+    print("YES")
+else:
+    print("NO")
